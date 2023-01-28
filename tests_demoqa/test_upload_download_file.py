7212271bb0043876953371655d329fbcaf295abe
DavidOwervill <jwh4davidik@gmai.com>
2023-01-28 16:16:16 +0700

    28/01/2023 тест1

--- tests_demoqa/test_upload_download_file.py
+++
@@ -1,7 +0,0 @@
-def test_upload_download_file(self):
-    driver = self.driver
-    driver.get(test_link_upload_download)
-    upload = driver.find_element(by=By.XPATH, value="//*[@id='uploadFile']")
-    time.sleep(5)
-    upload.send_keys("C:/Users/User/Desktop/GIT/Autotests/img/sampleFile.jpeg")
-    time.sleep(5)
